d1c755afe94b97612113bfa629c75e168cbd878e
[Brendan OConnor] <[boconnor@vmware.com]>
2024-11-13 14:22:04 -0500

    Init Python sdks for vsphere development

---
+++ photon/install_pyvmomi.sh
@@ -0,0 +1,4 @@
+# Preps Photon OS for pyvmomi
+# Prerequisite: _prep_photon.sh must be run first
+
+python3 -m pip install --upgrade pyvmomi
---
+++ photon/install_vsphere_py_sdk.sh
@@ -0,0 +1,4 @@
+# Preps Photon OS for vSphere Python SDK
+# Prerequisite: _prep_photon.sh must be run first
+
+python3 -m pip install --upgrade git+https://github.com/vmware/vsphere-automation-sdk-python.git
