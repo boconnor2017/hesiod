31803eda92ff73d22e7bd80a43264bbe4a14bdee
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2025-07-23 11:39:56 -0400

    docker-compose

--- photon/_prep_photon.sh
+++ photon/_prep_photon.sh
@@ -23,6 +23,12 @@ yum -y install https://repo.ius.io/ius-release-el$(rpm -E '%{rhel}').rpm
 # Install python3
 yum -y install python3
 python3 --version
+# Install docker compose
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.38.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+docker compose version
 # Create drop folder
 mkdir /usr/local/drop
 # Clone this repository
